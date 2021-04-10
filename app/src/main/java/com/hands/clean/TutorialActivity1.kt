8014d057e51c79cd65b004a040c3ae0d8d381b46
c970112 <c970112@naver.com>
2021-04-10 17:28:24 +0900

    다음버튼 제작 및 다음화면 제작 예정

---
+++ app/src/main/java/com/hands/clean/TutorialActivity1.kt
@@ -0,0 +1,16 @@
+package com.hands.clean
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_tutorial.*
+import kotlinx.android.synthetic.main.activity_tutorial1.*
+
+
+class TutorialActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial1)
+
+    }
+}
