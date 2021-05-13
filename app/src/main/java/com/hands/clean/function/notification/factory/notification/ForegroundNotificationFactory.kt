504de7eba783c5fa15938662533c69112b5af7fc
smworld01 <smworld01@naver.com>
2021-05-13 12:02:39 +0900

    add stop button in foreground notification

--- app/src/main/java/com/hands/clean/function/notification/factory/notification/ForegroundNotificationFactory.kt
+++ app/src/main/java/com/hands/clean/function/notification/factory/notification/ForegroundNotificationFactory.kt
@@ -1,23 +1,45 @@
 package com.hands.clean.function.notification.factory.notification
 
 import android.app.Notification
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.hands.clean.R
 import com.hands.clean.function.notification.type.NotifyInfo
+import com.hands.clean.function.receiver.ServiceStopReceiver
+import com.hands.clean.function.service.WashLocationServiceManager
 
 class ForegroundNotificationFactory(
     private val context: Context,
-    private val notifyInfo: NotifyInfo) : NotificationFactory {
-
+    private val notifyInfo: NotifyInfo
+) : NotificationFactory {
+    private val service = WashLocationServiceManager(context)
     override fun onBuild(): Notification {
         val builder: NotificationCompat.Builder =
             NotificationCompat.Builder(context, notifyInfo.channelId)
                 .setSmallIcon(R.drawable.ic_baseline_home_24)
                 .setContentTitle("WiFi와 GPS를 확인하고 있습니다.")
                 .setContentText("테스트 중..")
+                .addAction(getAction())
                 .setDefaults(Notification.FLAG_NO_CLEAR)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         return builder.build()
+    }
+
+    private fun getAction(): NotificationCompat.Action? {
+        val intent: Intent = getIntent()
+        val pendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        return NotificationCompat.Action.Builder(
+            R.drawable.ic_baseline_home_24, "중지", pendingIntent
+        )
+            .build()
+    }
+
+    private fun getIntent(): Intent {
+        return Intent(context, ServiceStopReceiver::class.java)
     }
 }
---
+++ app/src/main/java/com/hands/clean/function/receiver/ServiceStopReceiver.kt
@@ -0,0 +1,19 @@
+package com.hands.clean.function.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.hands.clean.function.service.WashLocationServiceManager
+import com.hands.clean.function.settings.WashSettingsManager
+
+class ServiceStopReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val serviceManager = WashLocationServiceManager(context)
+        val washSettings = WashSettingsManager(context)
+
+        washSettings.gpsNotify = false
+        washSettings.wifiNotify = false
+
+        serviceManager.onStopService()
+    }
+}
