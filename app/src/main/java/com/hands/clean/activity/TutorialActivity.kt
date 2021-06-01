5520f6a4766645d9576a3799558d736d481bcb3c
c970112 <c970112@naver.com>
2021-06-01 12:51:11 +0900

    튜토리얼 문구추가

--- app/src/main/java/com/hands/clean/activity/TutorialActivity.kt
+++ app/src/main/java/com/hands/clean/activity/TutorialActivity.kt
@@ -21,7 +21,10 @@ class TutorialActivity : AppCompatActivity() {
 
         val pager:ViewPager2 = findViewById(R.id.pager1)
         val dotsIndicator = findViewById<WormDotsIndicator>(R.id.dots_indicator)
-        val stringList: List<String> = arrayListOf("안녕하세요\n" + "튜토리얼 입니다.", "튜토리얼입니다.", "끝입니다.")
+        val stringList: List<String> = arrayListOf("안녕하세요\n" + "손씻기 어플에 대한 튜토리얼 입니다.", "우선 와이파이에 대한 설정입니다.\n" +
+                "와이파이에 관한 알림설정 및 \n" + "기기관리가 가능합니다.", "이 다음은 블루투스에 관한 설명입니다.\n" + "블루투스에 대한 알림설정과 등록, 기기관리가 \n" + "가능합니다.","GPS에 관한 설명입니다.\n"
+                + "GPS에 대한 알림설정과 등록된 위치 관리\n" + "위치찾기가 가능합니다.\n" + "저희 어플이 다른 위치권한과 다른점은 \n"
+                + "위치권한을 사용하지만 인터넷을 사용하지 않아 \n " + "안전합니다.", "이상으로 튜토리얼을 마치겠습니다.\n" + "감사합니다.")
         pager.adapter = PagerRecyclerAdapter(stringList as ArrayList<String>)
         pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         dotsIndicator.setViewPager2(pager)
